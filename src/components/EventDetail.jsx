0101f46bfa66144c03e23e8308d0a3b904b57624
writeens <44687555+writeens@users.noreply.github.com>
2020-09-16 09:12:17 +0100

    Complete Event Details Page

--- src/components/EventDetail.jsx
+++ src/components/EventDetail.jsx
@@ -14,15 +14,22 @@ const EventDetail = () => {
   const [bg, setBg] = useState(null);
   const [title, setTitle] = useState('');
   const [organizer, setOrganizer] = useState('');
+  const [eventLink, setEventLink] = useState('');
+  const [longDesc, setLongDesc] = useState('');
+  const [shortDesc, setShortDesc] = useState('');
+  const [location, setLocation] = useState('');
 
   /** Handler to get Event Details and Update State */
   const getEventDetails = () => {
     const event = events.filter((item) => item.id === id)[0];
     if (event) {
-      console.log(event);
       setBg(event.image);
       setTitle(event.title);
       setOrganizer(event.organizer);
+      setEventLink(event.link);
+      setLongDesc(event.longDesc);
+      setShortDesc(event.shortDesc);
+      setLocation(event.location);
     }
   };
 
@@ -45,9 +52,9 @@ const EventDetail = () => {
           {id}
         </div>
         <div className="md:mx-32 lg:mx-48 mt-20 z-10 flex flex-col">
-          <div className="flex md:flex-row flex-col">
+          <div className="flex md:flex-row flex-col mb-4">
             <img style={{ maxHeight: '450px' }} src={bg} alt="event" className="w-full md:w-2/3" />
-            <div className="bg-gray-300 flex-col w-full flex justify-center content-center items-center md:h-64 h-full py-8 md:py-40">
+            <div className="bg-white flex-col w-full flex justify-center content-center items-center md:h-64 h-full py-8 md:py-40 shadow-xl rounded-sm">
               <p className="text-blue-900 font-bold text-center text-md">{title}</p>
               <p className="flex text-center font-medium text-gray-700 md:mb-16 mb-8">
                 {`by ${organizer}`}
@@ -59,6 +66,21 @@ const EventDetail = () => {
                 Reserve Ticket(s)
 
               </button>
+            </div>
+          </div>
+          <div className="flex flex-col bg-white p-12 shadow-xl rounded">
+            <div className="flex justify-between mb-8">
+              <p className="w-3/5 text-sm font-medium">{shortDesc}</p>
+              <div className="w-1/3">
+                <p className="text-lg font-bold">Location</p>
+                <p className="text-sm font-medium">{location}</p>
+              </div>
+            </div>
+            <div className="text-gray-600">
+              <p className="text-blue-900 text-lg font-bold mb-4">About Event</p>
+              <p className="mb-8">{longDesc}</p>
+              <p className="text-lg font-bold text-blue-900 mb-4">Event Link</p>
+              <a className="text-blue-500" href={eventLink}>{eventLink}</a>
             </div>
           </div>
         </div>
